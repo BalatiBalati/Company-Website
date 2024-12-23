83ea47f0102ad8c8e6a900effa50d2aa4941dbb0
Balati Balati <bbalati01@gmail.com>
2024-12-23 11:44:56 +0400

    Update

--- frontend_react/src/sanity/client.js
+++ frontend_react/src/sanity/client.js
@@ -2,13 +2,6 @@ import {createClient} from '@sanity/client';
 
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = createClient({
-  projectId: 'v1f4ktqd',
-  dataset: 'production',
-  apiVersion: '2024-09-15',
-  useCdn: 'true',
-  token: 'skpAjD9dtBeewwXttvz7oezXC5gS793vG1I399Um7SefiQveYHsDV7A2YpKuXOhbQUrckG9OD80EWtO8GQFrXWRIySrPr4R4wcHsfjkpwoaHFe4ujcMWpKEuEPZp5v5sOZ10TJJJtDld7g2po4spfyKRNJjov7BryQROgD0SIOvgfP8HbU5K',
-});
 
 const builder = imageUrlBuilder(client);
 
