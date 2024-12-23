d1a142d0898fe52a5765f39dcc7b0730444b119a
Balati Balati <bbalati01@gmail.com>
2024-12-23 11:45:35 +0400

    Update

--- frontend_react/src/sanity/client.js
+++ frontend_react/src/sanity/client.js
@@ -3,7 +3,6 @@ import {createClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 
-
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source)
