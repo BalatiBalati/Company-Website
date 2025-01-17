17f5cd2390a520457fd90c1c07a323b166b0421a
Balati Balati <bbalati01@gmail.com>
2025-01-17 20:27:34 +0400

    Remove unused CustomCursor and MouseTrackingEffect components and their styles; update Home.scss for improved text color.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -4,16 +4,10 @@ import "./App.scss";
 import { Home, About, Footer, Work, Skills} from "./container";
 import { Navbar } from "./components";
 import AnimatedBlob from "./components/AnimatedBlob/AnimatedBlob";
-import CustomCursor from "./components/CustomCursor/CustomCursor";
-import MouseTrackingEffect from "./components/MouseTrackingEffect/MouseTrackingEffect";
 
 const App = () => {
   return (
     <div className="app relative w-screen h-screen overflow-hidden">
-      {/* Mouse tracking effect */}
-      {/*<MouseTrackingEffect />*/}
-      {/* Custom Cursor */}
-      {/*<CustomCursor />*/}
       
       {/* Render animated blobs in a separate container */}
       <div className="blob-container">
--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -2,5 +2,4 @@ export {default as Navbar} from './Navbar/Navbar'
 export {default as SocialMedia} from './SocialMedia' 
 
 export { default as AnimatedBlob } from './AnimatedBlob/AnimatedBlob';
-export { default as CustomCursor } from './CustomCursor/CustomCursor';
-export { default as MouseTrackingEffect } from './MouseTrackingEffect/MouseTrackingEffect';
+
