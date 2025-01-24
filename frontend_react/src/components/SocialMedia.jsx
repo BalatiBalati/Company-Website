e3860f0c449bdb7b62228a442dd2f891a58055b8
Balati Balati <bbalati01@gmail.com>
2025-01-24 22:38:17 +0400

    Update SocialMedia component to wrap icons in anchor tags for improved accessibility and navigation

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsInstagram, BsLinkedin, BsGithub} from 'react-icons/bs';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-        <div>
-            <BsLinkedin />
-        </div>
+      <div>
+        <a href="https://www.linkedin.com/in/balati-balati-858b47318/" target="_blank" rel="noopener noreferrer">
+          <BsLinkedin />
+        </a>
+      </div>
 
-        <div>
-            <FaFacebookF />
-        </div>
+      <div>
+        <a href="https://www.instagram.com/_chipukizi_/" target="_blank" rel="noopener noreferrer">
+          <BsInstagram />
+        </a>
+      </div>
 
-        <div>
-            <BsInstagram />
-        </div>
-
-        <div>
-            <BsGithub />
-        </div>
-      
+      <div>
+        <a href="https://github.com/BalatiBalati" target="_blank" rel="noopener noreferrer">
+          <BsGithub />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SocialMedia;
