daeaf6ec56e5e204e284ef66592690fa25501881
Balati Balati <bbalati01@gmail.com>
2025-01-25 12:04:57 +0400

    Remove backend Sanity configuration and related files

--- backend_sanity/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-import {defineCliConfig} from 'sanity/cli';
-import imageUrlBuilder from '@sanity/image-url';
-
-export default defineCliConfig({
-  api: {
-    projectId: 'v1f4ktqd',
-    dataset: 'production',
-  },
-})
